0c9f2c0473f0ede586034e8bbe8f9fbaea9c395e
Nathan Brennan <MrNathanBrennan@gmail.com>
2020-05-18 12:46:16 +1000

    Clean up WeatherController, accept get params

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -7,14 +7,28 @@ use Illuminate\Support\Facades\Http;
 
 class WeatherController extends Controller
 {
-    public function getWeatherByCity($cityName)
+    public function index(Request $request)
     {
-        $days = 5;
-        $apiKey = env('WEATHER_API_KEY');
-        $weatherApiUrl = env('WEATHER_API_URL');
-        $url = $weatherApiUrl . '?key=' . $apiKey . '&city=' . $cityName . ',au&days=' . $days;
+        $openWeatherResponse = Http::get(env('WEATHER_API_URL'), [
+            'key' => env('WEATHER_API_KEY'),
+            'days' => $request->input('days', '5'),
+            'city' => $request->input('city', 'Brisbane,AU'),
+        ]);
 
-        $openWeatherResponse = Http::get($url);
+        if ($openWeatherResponse->failed()) {
+            return response()->json(['error' => 'Call to weather supplier failed'], 500);
+        }
+
+        return $openWeatherResponse->json();
+    }
+
+    public function getWeatherByCity($city)
+    {
+        $openWeatherResponse = Http::get(env('WEATHER_API_URL'), [
+            'key' => env('WEATHER_API_KEY'),
+            'city' => $city,
+            'days' => 5,
+        ]);
 
         if ($openWeatherResponse->failed()) {
             return response()->json(['error' => 'Call to weather supplier failed'], 500);
